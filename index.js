78052393d4bfded5c8262f67688ecee4901aaef5
dinhcaradoc <kwambugualex@gmail.com>
2025-03-20 20:55:03 +0300

    Update index.js
    - Configure middlewares
    - Add new routes for handling payment processes

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const connectDb = require('./config/db')
 const ussd = require('./ussd');
+const intaCB = require('./intasend');
+
+require('dotenv').config()
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to the database
+connectDb()
+  .then(() => console.log('Successfully connected to the database'))
+  .catch(err => console.error('There was an error connecting to MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.send("GET Request Called")
 })
 
+app.post('/intaResponse', intaCB.handleCB);
+
+// app.get('/tokenCheck', mpesa.generateToken)
 app.post('/ussd', ussd.initUssd);
 
 const port = 3004;
